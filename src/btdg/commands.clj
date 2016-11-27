59980fedda63bbad97c5310c9f36355357525f56
Benny Tsai <benny.tsai@gmail.com>
2016-11-27 00:45:02 -0500

    misc. bug fixes

--- src/btdg/commands.clj
+++ src/btdg/commands.clj
@@ -1,24 +1,29 @@
 (ns btdg.commands
-  (:require [btdg.config :as cfg]))
+  (:require [btdg.config :as cfg]
+            [repl-games.random :as rand]))
 
 ;; helpers
 
-(defn- get-player-idxs [game]
-  (-> game :state :players count range))
+(defn- get-in-game [game ks]
+  (get-in game (cons :state ks)))
 
-(defn- get-player-k [game player-idx k]
-  (get-in game [:state :players player-idx k]))
+(defn- assoc-in-game [game ks v]
+  (assoc-in game (cons :state ks) v))
 
-(defn- update-player-k [game player-idx k f & args]
-  (apply update-in game [:state :players player-idx k] f args))
+(defn- update-in-game [game ks f & args]
+  (apply update-in game (cons :state ks) f args))
 
-(defn- remove-player [game player-idx]
-  (update-in game [:state :players] #(->> (assoc % player-idx nil)
-                                          (remove nil?)
-                                          (vec))))
+(defn- get-in-player [game player-idx ks]
+  (get-in-game game (concat [:players player-idx] ks)))
+
+(defn- assoc-in-player [game player-idx ks v]
+  (assoc-in-game game (concat [:players player-idx] ks) v))
+
+(defn- update-in-player [game player-idx ks f & args]
+  (apply update-in-game game (concat [:players player-idx] ks) f args))
 
 (defn- roll-die []
-  (case (rand-int 6)
+  (case (rand/uniform 0 6)
     0 "ARROW"
     1 "DYNAMITE"
     2 "1"
@@ -32,11 +37,11 @@
   (let [n (:dice-count cfg/defaults)]
     (-> game
         ;; set # dice rolls to 1
-        (assoc-in [:state :dice-rolls] 1)
+        (assoc-in-game [:dice-rolls] 1)
         ;; roll all dice
-        (assoc-in [:state :dice] (vec (repeatedly n roll-die)))
+        (assoc-in-game [:dice] (vec (repeatedly n roll-die)))
         ;; mark all dice as active
-        (assoc-in [:state :active-die-idxs] (set (range n))))))
+        (assoc-in-game [:active-die-idxs] (set (range n))))))
 
 (defn roll-dice
   ([game]
@@ -46,12 +51,12 @@
   ([game & die-idxs]
    (reduce (fn [g die-idx]
              ;; roll selected die
-             (assoc-in g [:state :dice die-idx] (roll-die)))
+             (assoc-in-game g [:dice die-idx] (roll-die)))
            (-> game
                ;; increment # dice rolls
-               (update-in [:state :dice-rolls] inc)
+               (update-in-game [:dice-rolls] inc)
                ;; mark selected dice as active
-               (assoc-in [:state :active-die-idxs] (set die-idxs)))
+               (assoc-in-game [:active-die-idxs] (set die-idxs)))
            die-idxs)))
 
 (defn take-arrows
@@ -61,35 +66,35 @@
   ([game player-idx n]
    (let [arrows (-> n
                     ;; can't take more arrows than are left
-                    (min (get-in game [:state :arrows])))]
+                    (min (get-in-game game [:arrows])))]
      (-> game
-         (update-in [:state :arrows] - arrows)
-         (update-player-k player-idx :arrows + arrows)))))
+         (update-in-game [:arrows] - arrows)
+         (update-in-player player-idx [:arrows] + arrows)))))
 
 (defn discard-arrows
   ([game player-idx]
-   (let [arrows (get-player-k game player-idx :arrows)]
+   (let [arrows (get-in-player game player-idx [:arrows])]
      ;; by default, discard all arrows
      (discard-arrows game player-idx arrows)))
   ([game player-idx n]
    (let [arrows (-> n
                     ;; can't discard more arrows than player has
-                    (min (get-player-k game player-idx :arrows)))]
+                    (min (get-in-player game player-idx [:arrows])))]
      (-> game
-         (update-player-k player-idx :arrows - arrows)
-         (update-in [:state :arrows] + arrows)))))
+         (update-in-player player-idx [:arrows] - arrows)
+         (update-in-game [:arrows] + arrows)))))
 
 (defn gain-life
   ([game player-idx]
    ;; by default, gain 1 life
    (gain-life game player-idx 1))
   ([game player-idx n]
-   (let [max-life (get-player-k game player-idx :max-life)
+   (let [max-life (get-in-player game player-idx [:max-life])
          add-life #(-> %
                        (+ n)
                        ;; can't gain more than player max life
                        (min max-life))]
-     (update-player-k game player-idx :life add-life))))
+     (update-in-player game player-idx [:life] add-life))))
 
 (defn lose-life
   ([game player-idx]
@@ -100,21 +105,20 @@
                           (- n)
                           ;; can't go below 0 life
                           (max 0))
-         updated (update-player-k game player-idx :life remove-life)]
+         updated (update-in-player game player-idx [:life] remove-life)]
      (cond-> updated
-       ;; if player is dead, discard arrows and remove from game
-       (-> updated (get-player-k player-idx :life) zero?)
-       (-> (discard-arrows player-idx)
-           (remove-player player-idx))))))
+       ;; if player is dead, discard arrows
+       (-> updated (get-in-player player-idx [:life]) zero?)
+       (discard-arrows player-idx)))))
 
 (defn indians-attack
   ([game]
-   (let [player-idxs (get-player-idxs game)]
+   (let [player-idxs (-> game (get-in-game [:players]) count range)]
      ;; by default, attack all players
      (apply indians-attack game player-idxs)))
   ([game & player-idxs]
    (reduce (fn [g player-idx]
-             (let [arrows (get-player-k g player-idx :arrows)]
+             (let [arrows (get-in-player g player-idx [:arrows])]
                (-> g
                    (discard-arrows player-idx)
                    (lose-life player-idx arrows))))
@@ -123,12 +127,12 @@
 
 (defn gatling-gun
   ([game]
-   (let [active-player-idx (-> game :state :active-player-idx)
-         player-idxs (->> game get-player-idxs (remove #{active-player-idx}))]
+   (let [active-player-idx (get-in-game game [:active-player-idx])
+         player-idxs (-> game (get-in-game [:players]) count range)]
      ;; by default, attack each OTHER player
-     (apply gatling-gun game player-idxs)))
+     (apply gatling-gun game (remove #{active-player-idx} player-idxs))))
   ([game & player-idxs]
-   (let [active-player-idx (-> game :state :active-player-idx)]
+   (let [active-player-idx (get-in-game game [:active-player-idx])]
      (reduce (fn [g player-idx]
                (lose-life g player-idx))
              ;; discard arrows for active player
@@ -136,9 +140,12 @@
              player-idxs))))
 
 (defn end-turn [game]
-  (let [n (-> game :state :players count)]
-    (-> game
+  (let [n (-> game (get-in-game [:players]) count)
         ;; set next player as active
-        (update-in [:state :active-player-idx] #(-> % inc (mod n)))
-        ;; re-init dice
-        (init-dice))))
+        updated (update-in-game game [:active-player-idx] #(-> % inc (mod n)))
+        ;; get new active player idx
+        active-player-idx (get-in-game updated [:active-player-idx])]
+    (cond-> updated
+      ;; re-init dice iff new active player is alive
+      (-> updated (get-in-player active-player-idx [:life]) pos?)
+      (init-dice))))
--- src/btdg/print.clj
+++ src/btdg/print.clj
@@ -1,27 +1,36 @@
 (ns btdg.print
   (:require [clojure.string :as str]))
 
-(defn- print-player! [active-player-idx idx player]
-  (println (format "%s[%s] %s (LIFE %d/%d) (ARROWS %d)"
-                   (if (= active-player-idx idx)
-                     ">"
-                     " ")
-                   idx
-                   (-> player :role name str/upper-case)
-                   (-> player :life)
-                   (-> player :max-life)
-                   (-> player :arrows)))
-  (when (#{:sheriff :deputy} (-> player :role))
-    (println (format "     %s: %s"
-                     (-> player :name)
-                     (-> player :ability)))))
+(defn- ->active-marker [active-idxs idx]
+  (if (or (and (integer? active-idxs)
+               (= active-idxs idx))
+          (and (set? active-idxs)
+               (contains? active-idxs idx)))
+    ">"
+    " "))
 
-(defn- print-die! [active-die-idxs idx die]
+(defn- print-player! [active-player-idx player-idx player]
+  (if-not (-> player :life pos?)
+    (println (format "%s[%s] DEAD"
+                     (->active-marker active-player-idx player-idx)
+                     player-idx))
+    (let [role (:role player)]
+      (println (format "%s[%s] %s (LIFE %d/%d) (ARROWS %d)"
+                       (->active-marker active-player-idx player-idx)
+                       player-idx
+                       (-> role name str/upper-case)
+                       (-> player :life)
+                       (-> player :max-life)
+                       (-> player :arrows)))
+      (when (#{:sheriff :deputy} role)
+        (println (format "     %s: %s"
+                         (-> player :name)
+                         (-> player :ability)))))))
+
+(defn- print-die! [active-die-idxs die-idx die]
   (println (format "%s[%s] %s"
-                   (if (active-die-idxs idx)
-                     ">"
-                     " ")
-                   idx
+                   (->active-marker active-die-idxs die-idx)
+                   die-idx
                    die)))
 
 (defn print-game! [game]
