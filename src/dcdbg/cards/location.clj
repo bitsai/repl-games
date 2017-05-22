af13ba17874d6fcb8950d7d031b2bb465bce17fe
Benny Tsai <benny.tsai@gmail.com>
2017-05-22 12:51:39 -0700

    update location cards

--- src/dcdbg/cards/location.clj
+++ src/dcdbg/cards/location.clj
@@ -27,6 +27,38 @@
     :ongoing "When you play your first Hero on each of your turns, draw a card."
     :copies 1}])
 
+(def forever-evil
+  [{:name "Belle Reve"
+    :cost 5
+    :victory 1
+    :ongoing "+1 Power for each Villain you play"
+    :copies 1}
+   {:name "Blackgate Prison"
+    :cost 4
+    :victory 1
+    :ongoing "Once during each of your turns, reveal the top card of your deck. If it's a Vulnerability or Weakness, destroy it and gain 1 VP."
+    :copies 1}
+   {:name "Central City"
+    :cost 4
+    :victory 1
+    :ongoing "+1 Power for each non-Kick Super Power you play."
+    :copies 1}
+   {:name "Earth-3"
+    :cost 6
+    :victory 1
+    :ongoing "Once during each of your turns, reveal the top card of your deck. If it's a Punch, destroy it and gain 1 VP."
+    :copies 1}
+   {:name "Happy Harbor"
+    :cost 5
+    :victory 1
+    :ongoing "+1 Power for each Hero you play."
+    :copies 1}
+   {:name "S.T.A.R. Labs"
+    :cost 5
+    :victory 1
+    :ongoing "+1 Power for each Equipment you play."
+    :copies 1}])
+
 (def crisis1
   [{:name "House of Mystery"
     :cost 4
@@ -40,5 +72,5 @@
     :copies 1}])
 
 (def all
-  (->> (concat base crisis1)
+  (->> (concat base forever-evil crisis1)
        (map #(assoc % :type :location))))
