484ee52de61abe93ccf95eb30eb81fcb6426fd14
Benny Tsai <benny.tsai@gmail.com>
2017-07-20 23:53:10 -0700

    add starting gear + tokens

--- src/sob/cards/hero.clj
+++ src/sob/cards/hero.clj
@@ -80,10 +80,48 @@
                 :sanity 10
                 :will-power 4}})
 
+(def starting-gear
+  {:dynamite-satchel {:name "Dynamite Satchel"
+                      :keywords ["Gear" "Container"]
+                      :text "Holds up to 5 Dynamite Tokens."
+                      :keyword-restriction "Outlaw Only"
+                      :weight 1
+                      :gold-value 400}
+   :hold-out-pistol {:name "Hold-Out Pistol"
+                     :keywords ["Gear" "Gun" "Pistol" "Light"]
+                     :text "Free Attack (Once per Fight). Critical Hit on 5 or 6."
+                     :range 3
+                     :shots 1
+                     :keyword-restriction "Performer Only"
+                     :weight 1
+                     :upgrade-slots 1
+                     :gold-value 200}
+   :marshal-badge {:name "Marshal Badge"
+                   :keywords ["Gear" "Law" "Icon"]
+                   :text "Once per Adventure, give all Heroes +2 Shots with a Gun or +2 Combat (they choose) during their next Activation."
+                   :keyword-restriction "Law Only"}
+   :pistol {:name "Pistol"
+            :keywords ["Gear" "Gun" "Pistol"]
+            :range 6
+            :shots 2
+            :weight 1
+            :upgrade-slots 2
+            :hands :single-handed
+            :gold-value 100}
+   :shotgun {:name "Shotgun"
+             :keywords ["Gear" "Gun" "Shotgun"]
+             :range 5
+             :shots 1
+             :text "Uses the D8 To Hit and for Damage (6, 7, or 8 count as a Critical Hit)."
+             :weight 1
+             :upgrade-slots 1
+             :hands :double-handed
+             :gold-value 300}})
+
 (def starting-upgrades
   {:bandido [{:name "Explosives Expert"
               :text "Use 2 Grit to gain a Dynamite Token."
-              :extra-starting-gear [:dynamite-satchel :dynamite-token :dynamite-token]}
+              :extra-starting-gear [:dynamite-satchel :dynamite :dynamite]}
              {:name "Swindler"
               :text "Anytime you draw a Loot card, you may discard it and draw a new one. You must keep the second card drawn. You are also +1 Combat."}
              {:name "Twin Guns"
@@ -107,3 +145,9 @@
                  :text "Use 1 Grit to Heal 3 Wounds or 3 Sanity from yourself or another Hero on your Map Tile (gain 5 XP for every Wound/Sanity Healed from another Hero in this way). You are +2 Sanity."}
                 {:name "Rolling Thunder"
                  :text "Anytime you kill an Enemy, you may Recover a Grit on the D6 roll of 4, 5, or 6."}]})
+
+(def tokens
+  {:dead-eye-shot {:name "Dead Eye Shot"
+                   :text "Discard a Dead Eye Shot Token to add +2 Damage to one of your Ranged Hits with a Gun. You may not use more than one Dead Eye Shot Token per Hit."}
+   :dynamite {:name "Dynamite"
+              :text "Discard to throw as a Ranged Attack. If missed To Hit roll, bounces D3 times before exploding (see Bouncing Diagram on reverse side). Exploding Dynamite: Models in the target space and adjacent take D6 Wounds each, ignoring Defense."}})
