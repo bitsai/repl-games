2cfe20b7b256bb2a7009a6e822553a7ab7ddd04f
Benny Tsai <benny.tsai@gmail.com>
2017-07-26 13:20:28 -0700

    simplify Warp Shell characters

--- src/icrpg/characters.clj
+++ src/icrpg/characters.clj
@@ -11,8 +11,7 @@
            "Duranium Blade"
            "Common Shield (+2 Armor, can be sacrificed to absorb ALL of one hit against you and be destroyed)"
            "Common Armor (+1 Armor)"
-           "Force Shield Module (Ignore any damage of 3 or less)"
-           "Impervium Plating (Make a STR Attempt to shrug off a single attack)"]}
+           "Force Shield Module (Ignore any damage of 3 or less)"]}
    {:class "Gunner"
     :bio-form "Geno"
     :stats {:str 0 :dex 7 :con 0 :int 0 :wis 0 :cha 0}
@@ -21,19 +20,15 @@
     :hearts 1
     :gear ["Weapon Kit (+2 Weapon Effort)"
            "Blast Pistol (6 capacity)"
-           "Blast Pistol (6 capacity)"
            "Common Armor (+1 Armor)"
-           "Burst Fire Unit (Choose one weapon, on a hit roll Effort three times, distribute between targets as desired)"
-           "Slam Loader (Reload a weapon in zero time)"]}
+           "Burst Fire Unit (Choose one weapon, on a hit roll Effort three times, distribute between targets as desired)"]}
    {:class "Zurin"
     :bio-form "Geno"
-    :stats {:str 0 :dex 0 :con 0 :int 4 :wis 4 :cha 0}
+    :stats {:str 0 :dex 0 :con 0 :int 0 :wis 7 :cha 0}
     :armor 11
-    :effort {:basic 0 :weapons 0 :magic 1 :ultimate 0}
+    :effort {:basic 0 :weapons 0 :magic 2 :ultimate 0}
     :hearts 1
     :gear ["Fire Stone (+1 Magic Effort)"
-           "Walking Stick"
            "Energy Bow"
            "Common Armor (+1 Armor)"
-           "WIS Spell: Healing Words (Attempt with WIS, heal with Magical Effort, must be heard to function)"
-           "INT Spell: Words Beyond Time (Alles ALL roll Ultimate Effort on their next success)"]}])
+           "WIS Spell: Healing Words (Attempt with WIS, heal with Magical Effort, must be heard to function)"]}])
