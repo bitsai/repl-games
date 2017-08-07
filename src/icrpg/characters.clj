b8f56cf8c3f5079e680a53ce5dc8d7e7c3e14313
Benny Tsai <benny.tsai@gmail.com>
2017-08-07 16:45:34 -0700

    flesh out Warp Shell characters

--- src/icrpg/characters.clj
+++ src/icrpg/characters.clj
@@ -1,34 +1,37 @@
 (ns icrpg.characters)
 
 (def warp-shell
-  [{:class "TANK"
+  [{:name "REINHARDT"
+    :class "TANK"
     :bio-form "MECHA"
     :stats {:str 7 :dex 0 :con 0 :int 0 :wis 0 :cha 0}
     :armor 13
     :effort {:basic 0 :weapons 2 :magic 0 :ultimate 0}
     :hearts 2
     :gear {"Weapon Kit" "+2 Weapon Effort"
-           "Duranium Blade" nil
+           "Hammer" nil
            "Common Shield" "+2 Armor (can be sacrificed to absorb ALL of one hit against you and be destroyed)"
            "Common Armor" "+1 Armor"
            "Force Shield Module" "Ignore any damage of 3 or less"}}
-   {:class "GUNNER"
+   {:name "MCCREE"
+    :class "GUNNER"
     :bio-form "GENO"
     :stats {:str 0 :dex 7 :con 0 :int 0 :wis 0 :cha 0}
     :armor 11
     :effort {:basic 0 :weapons 3 :magic 0 :ultimate 0}
     :hearts 1
     :gear {"Weapon Kit" "+2 Weapon Effort"
-           "Blast Pistol" "6 capacity"
+           "Gauss Pistol" "6 capacity"
            "Common Armor" "+1 Armor"
            "Burst Fire Unit" "Choose one weapon and modify it, on a hit roll Effort three times, distribute between targets as desired (note: if this weapon is lost or destroyed, the Burst Fire Unit is also lost)"}}
-   {:class "ZURIN"
+   {:NAME "MERCY"
+    :class "ZURIN"
     :bio-form "GENO"
     :stats {:str 0 :dex 0 :con 0 :int 0 :wis 7 :cha 0}
     :armor 11
     :effort {:basic 0 :weapons 0 :magic 2 :ultimate 0}
     :hearts 1
     :gear {"Fire Stone" "+1 Magic Effort"
-           "Energy Bow" nil
+           "Blast Pistol" "6 capacity"
            "Common Armor" "+1 Armor"
            "WIS Power: Healing Words" "Attempt with WIS, heal with Magical Effort, must be heard to function"}}])
