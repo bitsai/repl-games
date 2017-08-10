08fe92e4ad17da579686a11d9137b3347934e9c8
Benny Tsai <benny.tsai@gmail.com>
2017-08-09 20:08:47 -0700

    upper-case names

--- src/btdg/characters.clj
+++ src/btdg/characters.clj
@@ -1,51 +1,51 @@
 (ns btdg.characters)
 
 (def base
-  [{:name "Bart Cassidy"
+  [{:name "BART CASSIDY"
     :max-life 8
     :ability "You may take an arrow instead of losing a life point (except to Indians or Dynamite)."}
-   {:name "Black Jack"
+   {:name "BLACK JACK"
     :max-life 8
     :ability "You may re-roll DYNAMITE (not if you roll three or more!)."}
-   {:name "Calamity Jane"
+   {:name "CALAMITY JANE"
     :max-life 8
     :ability "You can use 1 as 2 and vice-versa."}
-   {:name "El Gringo"
+   {:name "EL GRINGO"
     :max-life 7
     :ability "When a player makes you lose one or more life points, he must take an arrow."}
-   {:name "Jesse Jones"
+   {:name "JESSE JONES"
     :max-life 9
     :ability "If you have four life points or less, you gain two if you use BEER for yourself."}
-   {:name "Jourdonnais"
+   {:name "JOURDONNAIS"
     :max-life 7
     :ability "You never lose more than one life point to Indians."}
-   {:name "Kit Carlson"
+   {:name "KIT CARLSON"
     :max-life 7
     :ability "For each GATLING GUN you may discard one arrow from any player."}
-   {:name "Lucky Duke"
+   {:name "LUCKY DUKE"
     :max-life 8
     :ability "You may make one extra re-roll."}
-   {:name "Paul Regret"
+   {:name "PAUL REGRET"
     :max-life 9
     :ability "You never lose life points to the Gatling Gun."}
-   {:name "Pedro Ramirez"
+   {:name "PEDRO RAMIREZ"
     :max-life 8
     :ability "Each time you lose a life point, you may discard one of your arrows."}
-   {:name "Rose Doolan"
+   {:name "ROSE DOOLAN"
     :max-life 9
     :ability "You may use 1 or 2 for players sitting one place further."}
-   {:name "Sid Ketchum"
+   {:name "SID KETCHUM"
     :max-life 8
     :ability "At the beginning of your turn, any player of your choice gains one life point."}
-   {:name "Slab the Killer"
+   {:name "SLAB THE KILLER"
     :max-life 8
     :ability "Once per turn, you can use a BEER to double a 1 or 2."}
-   {:name "Suzy Lafayette"
+   {:name "SUZY LAFAYETTE"
     :max-life 8
     :ability "If you didn't roll any 1 or 2, you gain two life points."}
-   {:name "Vulture Sam"
+   {:name "VULTURE SAM"
     :max-life 9
     :ability "Each time another player is eliminated, you gain two life points."}
-   {:name "Willy the Kid"
+   {:name "WILLY THE KID"
     :max-life 8
     :ability "You only need two GATLING GUN to use the Gatling Gun."}])
