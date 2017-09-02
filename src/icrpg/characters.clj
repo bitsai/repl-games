ee1ed7ba866ecfcbcd1f02a96b3d939629cbd189
Benny Tsai <benny.tsai@gmail.com>
2017-09-02 10:08:14 -0700

    update Warp Shell characters

--- src/icrpg/characters.clj
+++ src/icrpg/characters.clj
@@ -1,21 +1,21 @@
 (ns icrpg.characters)
 
 (def warp-shell
-  [{:name "REINHARDT"
-    :class "TANK"
+  [{:class "TANK"
     :bio-form "MECHA"
+    :bio-form-abilities ["Can be rebuilt"]
     :stats {:str 7 :dex 0 :con 0 :int 0 :wis 0 :cha 0}
     :armor 13
     :effort {:basic 0 :weapons 2 :magic 0 :ultimate 0}
     :hearts 2
     :gear {"Weapon Kit" "+2 Weapon Effort"
-           "Hammer" nil
+           "Duranium Blade" nil
            "Common Shield" "+2 Armor (can be sacrificed to absorb ALL of one hit against you and be destroyed)"
            "Common Armor" "+1 Armor"
            "Force Shield Module" "Ignore any damage of 3 or less"}}
-   {:name "MCCREE"
-    :class "GUNNER"
+   {:class "GUNNER"
     :bio-form "GENO"
+    :bio-form-abilities []
     :stats {:str 0 :dex 7 :con 0 :int 0 :wis 0 :cha 0}
     :armor 11
     :effort {:basic 0 :weapons 3 :magic 0 :ultimate 0}
@@ -24,14 +24,14 @@
            "Gauss Pistol" "6 capacity"
            "Common Armor" "+1 Armor"
            "Burst Fire Unit" "Choose one weapon and modify it, on a hit roll Effort three times, distribute between targets as desired (note: if this weapon is lost or destroyed, the Burst Fire Unit is also lost)"}}
-   {:NAME "MERCY"
-    :class "ZURIN"
-    :bio-form "GENO"
-    :stats {:str 0 :dex 0 :con 0 :int 0 :wis 7 :cha 0}
-    :armor 11
-    :effort {:basic 0 :weapons 0 :magic 2 :ultimate 0}
+   {:class "ZURIN"
+    :bio-form "TORTON"
+    :bio-form-abilities ["Slow and steady" "Turtle mode"]
+    :stats {:str 0 :dex 0 :con 1 :int 0 :wis 7 :cha 0}
+    :armor 12
+    :effort {:basic 0 :weapons 0 :magic 1 :ultimate 0}
     :hearts 1
     :gear {"Fire Stone" "+1 Magic Effort"
-           "Blast Pistol" "6 capacity"
+           "Energy Bow" nil
            "Common Armor" "+1 Armor"
            "WIS Power: Healing Words" "Attempt with WIS, heal with Magical Effort, must be heard to function"}}])
