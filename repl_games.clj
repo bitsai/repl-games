b9141af4cf66894ee27fab3573905c91922e0023
Benny Tsai <benny.tsai@gmail.com>
2018-11-15 00:05:20 -0800

    update compiled file

--- repl_games.clj
+++ repl_games.clj
@@ -514,13 +514,13 @@
   (printf "You are a valiant gunslinger with a health of %d, an agility of %d, and a focus of %d.\n" @*player-health* @*player-agility* @*player-focus*))
 
 (defn player-attack-start []
-  (println "Attack style: [s]nipe [d1|d2]ouble tap [f]an the hammer"))
+  (println "Attack style: [a]imed shot [d1|d2]ouble tap [f]an the hammer"))
 
 (defn player-attack-end []
   (swap! *player-attacks* dec)
   (game-loop-end))
 
-(defn s [x]
+(defn a [x]
   (let [y (+ 2 (randval (quot @*player-focus* 2)))]
     (swap! *monsters* update x #(monster-hit % y))
     (player-attack-end)))
@@ -653,10 +653,133 @@
           (swap! *player-health* - 2))
 
       (= x @*player-agility*)
-      (do (println "A brigand catches your arm with his whip, taking off 2 agility points!")
+      (do (println "A brigand catches your arm with his knife, taking off 2 agility points!")
           (swap! *player-agility* - 2))
 
       (= x @*player-focus*)
-      (do (println "A brigand cuts your head with his whip, taking off 2 focus points!")
+      (do (println "A brigand cuts your head with his knife, taking off 2 focus points!")
           (swap! *player-focus* - 2)))
     m))
+(ns supercrew.core
+  (:require [clojure.string :as str]))
+
+;; https://fortressofsolidrules.wordpress.com/2009/11/12/villain-points-by-blacksheep/
+
+(def avengers
+  {:heroes [{:name "Captain America"
+             :abilities ["Leadership 3"
+                         "Vibranium shield 2"
+                         "Super-Soldier 1"]
+             :tricks ["[ ] Heroism [re-roll]"
+                      "[ ] Reserve strength [Effect +1]"
+                      "[ ] Shield throw [Effect 2]"]}
+            {:name "Hulk"
+             :abilities ["Rage 3"
+                         "Super strength 2"
+                         "Brawler 1"]
+             :tricks ["[ ] Hulk stomp [re-roll]"
+                      "[ ] Fury [Effect +1]"
+                      "[ ] Hulk smash [Effect 2]"]}
+            {:name "Iron Man"
+             :abilities ["Alpha strike"
+                         "Iron Man armor 2"
+                         "J.A.R.V.I.S. 1"]
+             :tricks ["[ ] Speed overdrive [re-roll]"
+                      "[ ] Energy reroute [Effect +1]"
+                      "[ ] Uni-beam [Effect 2]"]}
+            {:name "Thor"
+             :abilities ["God of Thunder 3"
+                         "Mjolnir 2"
+                         "Asgardian warrior 1"]
+             :tricks ["[ ] God speed [re-roll]"
+                      "[ ] Thor's rage [Effect +1]"
+                      "[ ] Mjolnir slam [Effect 2]"]}]
+   :enemies {1 [{:name "Chitauri"
+                 :abilities ["Chitauri gun 1"]
+                 :ability-names ["Chitauri bomb"]}]
+             2 [{:name "Leviathan"
+                 :abilities ["Charge 2"]
+                 :ability-names ["Bite"
+                                 "Slam"]}]
+             3 [{:name "Loki"
+                 :abilities ["Scepter 3"]
+                 :ability-names ["Daggers"
+                                 "Illusions"
+                                 "Mind control"]}]}})
+
+(def templates
+  {1 {:rank 1
+      :toughness 2
+      :boosts 1}
+   2 {:rank 2
+      :toughness 3
+      :boosts 2}
+   3 {:rank 3
+      :toughness 5
+      :boosts 3}})
+
+(defn add-ability [{:keys [ability-names rank] :as enemy} ability-fn]
+  (let [[an & ans] (shuffle ability-names)]
+    (-> enemy
+        (update :abilities conj (ability-fn an rank))
+        (update :ability-names (constantly ans)))))
+
+(def boost-fns
+  [(fn [enemy]
+     (add-ability enemy #(format "[ ] %s %d" %1 (inc %2))))
+   (fn [enemy]
+     (add-ability enemy #(format "[ ] %s %d [gives the target a -1 penalty]" %1 %2)))
+   (fn [enemy]
+     (add-ability enemy #(format "[ ] %s %d [attacks two targets]" %1 %2)))
+   (fn [enemy]
+     (add-ability enemy #(format "[ ] %s [provides a two-dice defense]" %1 %2)))
+   (fn [enemy]
+     (update enemy :toughness + 2))])
+
+(defn init-enemy [enemy template]
+  (loop [[bf & bfs] (shuffle boost-fns)
+         {:keys [boosts] :as e} (merge enemy template)]
+    (if (zero? boosts)
+      e
+      (recur bfs (-> e bf (update :boosts dec))))))
+
+(defn init-enemies [n enemies]
+  (loop [n n
+         es []]
+    (if (zero? n)
+      es
+      (let [x (-> (min n 3) rand-int inc)
+            e (init-enemy (-> x enemies rand-nth) (templates x))]
+        (recur (- n x) (conj es e))))))
+
+(defn print-character [{:keys [abilities name toughness tricks]}]
+  (printf "%s\n" name)
+  (doseq [x (concat abilities tricks)]
+    (printf "%s\n" x))
+  (printf "Toughness %d\n" toughness))
+
+(defn print-hero [hero]
+  (print-character (assoc hero :toughness 3))
+  (println "[ ] Anecdote bonus | Hero points 0"))
+
+(defn ng [{:keys [heroes enemies]}]
+  (let [n 2]
+    (doseq [e (init-enemies (inc n) enemies)]
+      (print-character e)
+      (println))
+    (println "--\n")
+    (doseq [h (->> heroes shuffle (take n))]
+      (print-hero h)
+      (println))))
+
+(defn d [n sides]
+  (repeatedly n #(inc (rand-int sides))))
+
+(defn r [n]
+  (loop [n n
+         rs []]
+    (let [r (-> 6 rand-int inc)]
+      (cond
+        (zero? n) (printf "%s => Effect %d" rs (->> rs (filter #(> % 3)) count))
+        (= r 6)   (recur n (conj rs r))
+        :else     (recur (dec n) (conj rs r))))))
